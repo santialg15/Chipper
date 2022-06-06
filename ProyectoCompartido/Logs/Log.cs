86e46e78ce6441e0774d430ec3bcf0c5c82fd68a
dpena22 <denispemar@hotmail.com>
2022-06-06 12:35:20 -0300

    Log

---
+++ ProyectoCompartido/Logs/Log.cs
@@ -0,0 +1,30 @@
+﻿namespace ProyectoCompartido.Logs
+{
+    public class Log
+    {
+        public string user { get; set; }
+        public string action { get; set; }
+        public string message { get; set; }
+        public string send { get; set; }
+        public string receive { get; set; }
+
+        public Log(string _user, string _action, string _message)
+        {
+            user = _user;
+            action = _action;
+            message = _message;
+            send = DateTime.Now.ToString();
+            receive = "";
+        }
+
+        public Log()
+        {
+        }
+
+        public override string ToString()
+        {
+            return "[x] Usuario: " + user + ", Acción: " + action + ", Mensaje: " + message + ", Enviado: " + send +
+                   ", recivido en servidor: " + receive;
+        }
+    }
+}
