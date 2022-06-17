2b8f93004eccfbba7ac8e7f560c93862f8e7b003
dpena22 <denispemar@hotmail.com>
2022-06-16 21:29:04 -0300

    Delete rq

---
+++ LogServer.Exceptions/MethodDoesNotExist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogServer.Exceptions
+{
+    public class MethodDoesNotExist : Exception
+    {
+        public MethodDoesNotExist()
+            : base("El metodo no existe")
+        {
+        }
+    }
+}
